2f3ec3ae71ccb53644a8907501f823c9bb54c9ad
sehyun734 <101646633+sehyun734@users.noreply.github.com>
2025-08-25 00:31:39 +0900

    init

---
+++ entrypoints/components/AnnotationManager.jsx
@@ -0,0 +1,4 @@
+export default function AnnotationManager() {
+  console.log("annotation loaded");
+  return <div style={{ position: "fixed", zIndex: 9999999 }}>hello</div>;
+}
---
+++ entrypoints/content.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AnnotationManager from "./components/AnnotationManager";
+import "./styles/index.css";
+
+export default defineContentScript({
+  matches: ["<all_urls>"],
+  cssInjectionMode: "ui", // apply css only in shadow DOM
+
+  async main(ctx) {
+    const ui = await createShadowRootUi(ctx, {
+      name: "annotation-manager",
+      position: "overlay",
+      onMount: (container) => {
+        const root = ReactDOM.createRoot(container);
+        root.render(React.createElement(AnnotationManager)); // .js so just class
+        return root;
+      },
+      onRemove: (root) => root?.unmount(),
+    });
+
+    ui.mount();
+  },
+});
